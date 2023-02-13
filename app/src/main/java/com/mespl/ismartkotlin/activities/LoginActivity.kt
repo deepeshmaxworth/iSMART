3e997fca7c4151e0129e54a315d44137fd1d192a
Advitiya <advitiya@maxworthsystems.com>
2023-02-13 16:45:49 +0530

    Settings page password added

--- app/src/main/java/com/mespl/ismartkotlin/activities/LoginActivity.kt
+++ app/src/main/java/com/mespl/ismartkotlin/activities/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.mespl.ismartkotlin.activities
 
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
+import com.mespl.ismartkotlin.R
 import com.mespl.ismartkotlin.databinding.ActivityLoginBinding
 
 
@@ -22,7 +32,8 @@ class LoginActivity : BaseActivity() {
 
     private fun initView(binding: ActivityLoginBinding) {
         this.also { mCtx = it }
-        val btnlogin: LinearLayout = binding.btnlogin
+
+       val btnlogin: LinearLayout = binding.btnlogin
     }
 
 
@@ -31,7 +42,7 @@ class LoginActivity : BaseActivity() {
     }
 
     fun settingClick(view: View) {
-        startActivity(Intent(mCtx, SettingActivity::class.java))
+        callDialog()
     }
 
     fun langClick(view: View) {
@@ -40,5 +51,73 @@ class LoginActivity : BaseActivity() {
 
     fun syncClick(view: View) {
 
+    }
+
+    fun callDialog() {
+        val settingpswrd: EditText
+        val texOK: TextView
+        val texExit: TextView
+        val dialog = Dialog(this@LoginActivity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.settingpasword_dialog)
+        dialog.window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        settingpswrd = dialog.findViewById(R.id.pinView)
+        texOK = dialog.findViewById(R.id.al_button_ok)
+        texExit = dialog.findViewById(R.id.al_button_Exit)
+        dialog.setCancelable(false)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        settingpswrd.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 4) {
+                    val imm =
+                        this@LoginActivity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+                }
+            }
+        })
+        texOK.setOnClickListener(View.OnClickListener {
+            if (settingpswrd.text.toString().equals("2662", ignoreCase = true)) {
+                val intent = Intent(this@LoginActivity, SettingActivity::class.java)
+                startActivity(intent)
+                dialog.dismiss()
+                //                    finish();
+            } else {
+                if (!settingpswrd.text.toString().equals("2662", ignoreCase = true)) {
+                    promptErrorMessage(getString(R.string.Please_Enter_Correct_Password))
+                    return@OnClickListener
+                } else {
+                    promptErrorMessage(getString(R.string.Please_Enter_Password))
+                    return@OnClickListener
+                }
+            }
+        })
+        texExit.setOnClickListener {
+            dialog.dismiss()
+            dialog.hide()
+        }
+        dialog.show()
+    }
+
+    fun promptErrorMessage(caution: String?) {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.errordialog)
+        dialog.setCancelable(false)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setTitle("Message")
+        val texOK = dialog.findViewById<LinearLayout>(R.id.al_button_ok)
+        val dialogtextChange = dialog.findViewById<TextView>(R.id.al_txt_preview)
+        dialogtextChange.text = caution
+        dialog.show()
+        texOK.setOnClickListener { dialog.dismiss() }
     }
 }
